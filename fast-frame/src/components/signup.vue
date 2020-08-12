2fd4c6bf72fb84f86dd427c0ec5bf9fe204eeff2
ituyama <42466955+ituyama@users.noreply.github.com>
2020-08-12 23:30:28 +0900

    aaaaaas

---
+++ fast-frame/src/components/signup.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="signup">
+    <table>
+        <tr>
+          <th>メールアドレス：</th>
+        </tr>
+        <tr>
+          <td><input type="email" v-model="mailaddress"/></td>
+        </tr>
+        <tr>
+          <th>パスワード：</th>
+        </tr>
+        <tr>
+          <td><input type="password" v-model="password"/></td>
+        </tr>
+      </table>
+      <button @click="signUp">登録</button>
+  </div>
+</template>
+<script>
+  methods: {
+      signUp: function () {
+        firebase.auth().createUserWithEmailAndPassword(this.mailaddress, this.password)
+        .then(
+          // 成功時の処理
+        )
+        .catch(
+          // エラー時の処理
+        )
+      }
+    }
+
+</script>
--- fast-frame/src/main.js
+++ fast-frame/src/main.js
@@ -6,3 +6,21 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyAhufCR0MI0rzJgBUCwCKzhaEuuQJgV9Do",
+authDomain: "fast-frame.firebaseapp.com",
+databaseURL: "https://fast-frame.firebaseio.com",
+projectId: "fast-frame",
+storageBucket: "fast-frame.appspot.com",
+messagingSenderId: "20630914601",
+appId: "1:20630914601:web:ec2089c24389c0cbfefe38",
+measurementId: "G-8WNGR58074"
+
+};
+
+firebase.initializeApp(config);
